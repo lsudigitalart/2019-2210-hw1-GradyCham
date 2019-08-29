6b68b1baa924a1881567d257a6fe55d419d028a2
Grady Cunningham <gcunni4@lsu.edu>
2019-08-29 14:46:12 -0500

    Finished head, doing hair

--- sketch.js
+++ sketch.js
@@ -16,10 +16,30 @@ function draw (){
 	fill(0);
 	noStroke();
 	ellipse(700,400, 150, 250);
+	//Earring
+	fill(255);
+	stroke(0);
+	strokeWeight(3);
+	ellipse(80,520, 25, 75);
+	fill(255);
+	stroke(0);
+	strokeWeight(3);
+	ellipse(720,520, 25, 75);
 	//head
 	fill(0);
 	noStroke();
 	ellipse(400,400, 600, 600);
+	//Mouth
+	fill(255);
+	noStroke();
+	ellipse(400, 450, 500, 400);
+	fill(0);
+	noStroke();
+	ellipse(400,400, 500, 350);
+	//hair top
+	fill(255);
+	noStroke();
+	ellipse(275,200, 100, 100);
 	//eyes
 	fill(255);
 	noStroke();
@@ -33,5 +53,12 @@ function draw (){
 	fill(0);
 	noStroke();
 	ellipse(525,375, 150, 150);
+	//nose
+	fill(255);
+	noStroke();
+	ellipse(400,425, 150, 150);
+	fill(0);
+	noStroke();
+	ellipse(400,415, 150, 150);
 	
 }
